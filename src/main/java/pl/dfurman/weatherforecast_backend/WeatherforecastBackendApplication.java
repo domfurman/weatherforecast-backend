fec84d66f65cbbc818b4ca41088740d311580f84
Dominik Furman <domfurman01@gmail.com>
2024-12-06 14:09:19 +0100

    project creation

---
+++ src/main/java/pl/dfurman/weatherforecast_backend/WeatherforecastBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.dfurman.weatherforecast_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherforecastBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherforecastBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/dfurman/weatherforecast_backend/WeatherforecastBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dfurman.weatherforecast_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherforecastBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
