357300d129dcf4289d169a0f9f7b7344c684aded
Dominik Furman <domfurman01@gmail.com>
2024-12-09 14:45:34 +0100

    allow cross-origin requests

--- src/main/java/pl/dfurman/weatherforecast_backend/weather/WeatherController.java
+++ src/main/java/pl/dfurman/weatherforecast_backend/weather/WeatherController.java
@@ -1,11 +1,13 @@
 package pl.dfurman.weatherforecast_backend.weather;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class WeatherController {
 
     private final WeatherService weatherService;
